46db9e961cebfbcea07632d876788f90786203db
jennar305 <71038940+jennar305@users.noreply.github.com>
2021-10-14 15:29:57 -0400

    feed page works

--- client/src/services/session.js
+++ client/src/services/session.js
@@ -1,17 +1,20 @@
 import router from "../router";
 import { Login } from "./users";
+
 const session = {
     user: null,
     messages: [],
     toRoute: '/feed',
     Login(handle, password){
 
-        const user = Login(handle, password);
+        const response = Login(handle, password);
 
-        this.user = user.user;
+        this.user = response.user;
 
         router.push(this.toRoute);
+
     }
 };
 
 export default session;
+

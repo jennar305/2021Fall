fd4c1ab3e425102cf79641dde3107296a2107a90
jennar305 <71038940+jennar305@users.noreply.github.com>
2021-11-15 18:33:28 -0500

    catch server errors

--- client/src/services/myFetch.js
+++ client/src/services/myFetch.js
@@ -1,11 +1,28 @@
 import session from "./session";
 
-const API_ROOT = process.env.VUE_APP_APT_ROOT ?? 'http://localhost:3100/';
+const API_ROOT = process.env.VUE_APP_API_ROOT ?? 'http://localhost:3100/';
 
-export async function api(url){
+export async function api(url, data = null, method = null){
     try {
-        const x = await fetch(API_ROOT + url);
-        return await x.json();
+        let response;
+
+        if(data){
+            response = await fetch(API_ROOT + url, {
+                method: method ?? 'POST', // *GET, POST, PUT, DELETE, etc.
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data) // body data type must match "Content-Type" header
+              });
+        }else{
+            response = await fetch(API_ROOT + url);
+        }
+        
+        if(!response.ok){
+            throw await response.json();
+        }
+        return await response.json();
     } catch (err) {
         session.Error(err);
     }

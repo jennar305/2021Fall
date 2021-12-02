5436943745ffbcb375e485a20a981a9efa4ce4df
jennar305 <71038940+jennar305@users.noreply.github.com>
2021-12-02 18:08:47 -0500

    use state to make menu collapse properly

--- react-app/src/components/Nav.js
+++ react-app/src/components/Nav.js
@@ -1,36 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+
+import logo from '../assets/logo.svg';
+
+//import LoginBadge from './LoginBadge';
 
 export default function Nav() {
 
-    const data = { isActive: false }
+    const [isActive, setIsActive] = useState(false);
 
     return (
   <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
   <div className="navbar-brand">
     <a className="navbar-item" href="https://bulma.io">
-      <img src="../assets/logo.png" height="28" />
+      <img src={logo} className="App-logo" height="28" />
       <h3 className="title">Be Happy</h3>
     </a>
 
-    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" className="{'is-active': isActive }" click="isActive = !isActive" >
+    <a role="button" className={"navbar-burger" + ( isActive ? ' is-active' : '')} aria-label="menu" aria-expanded="false" onClick={()=> setIsActive(!isActive) }  >
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div className="navbar-menu " className="{'is-active': isActive }">
+  <div className={"navbar-menu" + ( isActive ? ' is-active' : '')}>
     <div className="navbar-start">
-      <NavLink className="navbar-item is-tab" to="/" active-className="is-active" exact>
+      <NavLink className={({isActive}) => "navbar-item is-tab" + ( isActive && " is-active")} to="/" exact>
         Home
       </NavLink>
 
-      <NavLink className="navbar-item is-tab" to="/feed" active-className="is-active">
+      <NavLink  className={({isActive}) => "navbar-item is-tab" + ( isActive && " is-active")}  to="/feed" >
         Feed
       </NavLink>
 
-      <NavLink className="navbar-item is-tab" to="/about" active-className="is-active">
+      <NavLink className={({isActive}) => "navbar-item is-tab" + ( isActive && " is-active")}  to="/about" >
         Documentation
       </NavLink>
 
@@ -40,7 +44,7 @@ export default function Nav() {
         </a>
 
         <div className="navbar-dropdown">
-          <NavLink className="navbar-item is-tab" to="about" active-className="is-active">
+          <NavLink  className={({isActive}) => "navbar-item is-tab" + ( isActive && " is-active")}  to="about" >
             About
           </NavLink>
           <a className="navbar-item">
